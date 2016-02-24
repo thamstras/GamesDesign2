d5fb41d6e568ca6ba20c6962e5f94bf3f0122cbf
Thamstras <thomas_casson@btinternet.com>
2016-02-24 15:29:23 +0000

    Initial Commit

---
+++ Assets/GatePickupScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GatePickupScript : MonoBehaviour {
+
+    Camera mainCam;
+
+    Transform parent;
+
+    public Vector3 attach;
+
+    GameObject gate;
+
+    bool haveGate = false;
+
+	// Use this for initialization
+	void Start () {
+        mainCam = GetComponentInChildren<Camera>();
+        parent = GetComponentInParent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Interact"))
+        {
+            Vector3 origin = mainCam.transform.position;
+            Vector3 direction = mainCam.transform.forward;
+            Ray ray = new Ray(origin, direction);
+            RaycastHit rayHit = new RaycastHit();
+            if (Physics.Raycast(ray, out rayHit, 100.0f))
+            {
+                if (rayHit.collider.gameObject.tag == "GateSocket")
+                {
+                    GateSocketScript socket = rayHit.collider.GetComponent<GateSocketScript>();
+                    if (socket.hasGate())
+                    {
+                        GameObject gate = socket.unsocket();
+                        gate.transform.SetParent(transform);
+                        Vector3 loc = transform.TransformPoint(attach);
+                        gate.transform.position = loc;
+                        haveGate = true;
+                    }
+                }
+            }
+        }
+	}
+}
---
+++ Assets/GateSocketScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateSocketScript : MonoBehaviour {
+
+    bool haveGate = false;
+
+    GameObject gate;
+
+	// Use this for initialization
+	void Start () {
+        foreach (Transform child in transform)
+        {
+            if (child.CompareTag("GateObject"))
+            {
+                gate = child.gameObject;
+                haveGate = true;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool hasGate()
+    {
+        return haveGate;
+    }
+
+    public GameObject unsocket()
+    {
+        GameObject temp = gate;
+        gate.transform.SetParent(transform.parent);
+        gate = null;
+        haveGate = false;
+        return temp;
+    }
+}
